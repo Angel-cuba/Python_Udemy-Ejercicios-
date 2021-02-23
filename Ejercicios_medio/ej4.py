e312ca3831a81e9da7922a13d0aae76148307e9b
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-23 20:18:55 +0200

    IMPORTANTE....encuentra el mayor de los # en una lista

--- Ejercicios_medio/ej4.py
+++ Ejercicios_medio/ej4.py
@@ -23,3 +23,27 @@ while a < y:
           if y % a == 0:
                     print(a, end=" ")
           a += 1
+
+# Dada una lista de numeros, mostrar el mayor de ellos
+print("\n")
+N = int(input("Ingresa la cantidad de numeros: "))
+aux = -1
+
+for v in range(N):
+          v = int(input("Ingresa los números: "))
+          if v > aux:
+                    aux = v
+print("El mayor es: ", aux)
+
+########Con una lista
+lista= []
+b = int(input("Ingresa cantidad: "))
+for item in range(b):
+          b_lista = int(input("Dame numeros: "))
+          lista.append(b_lista)
+aux1 = -1
+for item in lista:
+              if item > aux1:
+                            aux1 = item
+print(lista)
+print("El mayor es :", aux1)    
