ba12d42d08bc40287322723e7547cd887d06c697
Angel-cubita <angelito.cuba1984@gmail.com>
2021-02-24 19:01:08 +0200

    Buscar # de una lista en otra----Mostrar de un# su mayor dígito

--- Ejercicios_medio/ej6.py
+++ Ejercicios_medio/ej6.py
@@ -16,3 +16,18 @@ if mayor_a in b:
           print("El mayor de A está en B")
 else:
           print("El mayor de A no está en B")
+
+
+########## Mostrar el mayor de los dígitos
+mayor = -1
+numero = int(input("Dame número: "))
+while numero > 0:
+          if numero % 10 > mayor:
+                    mayor = numero % 10
+          numero = numero // 10
+print("El mayor es: ", mayor)
+
+################################################################
+mayor = -1
+numero = int(input("Ingresa número---:"))
+print("El mayor es: ", max(numero))
